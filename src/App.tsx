d126d0de0e1af8feee4cf36d0eb5b269b4cab8a2
Hubert Legęć <hubert.legec@gmail.com>
2019-02-03 00:40:56 +0100

    Not Found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import React, {Component, Fragment} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {MuiThemeProvider} from "@material-ui/core";
-import {Header} from "./components/header/Header";
-import {theme} from "./theme";
+import { MuiThemeProvider } from "@material-ui/core";
+import { Header } from "./components/header/Header";
+import { theme } from "./theme";
 import './App.css';
-import {LandingPage} from "./components/landingPage/LandingPage";
-import {RegistrationPage} from "./components/registrationPage/RegistrationPage";
+import { LandingPage } from "./components/landingPage/LandingPage";
+import { RegistrationPage } from "./components/registrationPage/RegistrationPage";
 import { PrivateRoute } from './components/PrivateRoute';
 import { SettingsPage } from './components/settingsPage/SettingsPage';
+import { NotFoundPage } from './components/notFound/NotFoundPage';
 
 export class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <CssBaseline/>
-                <MuiThemeProvider theme={theme}>
-                    <BrowserRouter>
-                        <div>
-                            <Header/>
-                            <Route path='/' exact component={LandingPage}/>
-                            <Route path='/registration' component={RegistrationPage}/>
-                            <PrivateRoute path='/settings' component={SettingsPage}/>
-                        </div>
-                    </BrowserRouter>
-                </MuiThemeProvider>
-            </Fragment>
-        );
-    }
+  render() {
+    return (
+      <Fragment>
+        <CssBaseline/>
+        <MuiThemeProvider theme={theme}>
+          <BrowserRouter>
+            <div>
+              <Header/>
+              <Switch>
+                <Route path='/' exact component={LandingPage}/>
+                <Route path='/registration' component={RegistrationPage}/>
+                <PrivateRoute path='/settings' component={SettingsPage}/>
+                <Route component={NotFoundPage}/>
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </Fragment>
+    );
+  }
 }
