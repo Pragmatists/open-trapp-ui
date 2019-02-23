148e5a1e4d88df5f3355135e7b5d521e80109d35
Hubert Legęć <hubert.legec@gmail.com>
2019-02-23 23:05:00 +0100

    Remove redux logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,6 @@
-import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { rootReducer } from './root.reducer';
-
-const loggerMiddleware = createLogger();
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -11,8 +8,7 @@ export const store = createStore(
   rootReducer,
   composeEnhancers(
     applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
+      thunkMiddleware
     )
   )
 );
