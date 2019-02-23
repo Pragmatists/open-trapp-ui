16a59efd7d22402c3bdac01632c27bececbd1aef
Hubert Legęć <hubert.legec@gmail.com>
2019-02-23 23:05:34 +0100

    OpenTrapp API - token interceptor

--- src/api/dtos.d.ts
+++ src/api/dtos.d.ts
@@ -1,9 +1,28 @@
 
 export interface AuthorizedUser {
-  token: string;
-  email: string;
-  name: string;
-  displayName: string;
-  profilePicture: string;
-  roles: string[];
+  readonly token: string;
+  readonly email: string;
+  readonly name: string;
+  readonly displayName: string;
+  readonly profilePicture: string;
+  readonly roles: string[];
+}
+
+export interface MonthDTO {
+  readonly id: string;
+  readonly link: string;
+  readonly next: LinkDTO;
+  readonly prev: LinkDTO;
+  readonly days: DayDTO[];
+}
+
+interface DayDTO {
+  readonly id: string;
+  readonly link: string;
+  readonly weekend: boolean;
+  readonly holiday: boolean;
+}
+
+interface LinkDTO {
+  readonly link: string;
 }
