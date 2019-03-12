f001f176974ee715796b7c1568f4e356180ccda1
Hubert Legęć <hubert.legec@gmail.com>
2019-03-13 00:21:34 +0100

    unused Month class removed

--- src/time/TimeProvider.ts
+++ src/time/TimeProvider.ts
@@ -1,16 +1,6 @@
-import moment from 'moment';
-import {Month} from "./Month";
 
 export class TimeProvider {
     getCurrentDate() {
         return new Date();
-    }
-
-    get moment() {
-        return moment();
-    }
-
-    get currentMonth() {
-        return Month.fromDate(this.moment);
     }
 }
